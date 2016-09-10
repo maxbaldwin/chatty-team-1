0acc38ea20d0cfc75c18ef553d9c6db2bac5fcf6
Bryan Hankins <bryanhankins@me.com>
2016-09-10 13:29:05 -0500

    updated json and js that coresponds

--- json.js
+++ json.js
@@ -1,1 +1,29 @@
-json.js
+var Chatty = (function(messages) {
+  var messageData = [];
+  var currentMessage;
+
+  function loadMessages () {
+    messageData = JSON.parse(this.response);
+    var messageElement = document.getElementById("messageOutput");
+      for (var i = 0; i < messageData.messages.length; i++) {
+        currentMessage = messageData.messages[i];
+        console.log(currentMessage)
+
+        currentMessage += `<div>Basic User Message: ${currentMessage.userMessage}</div>`;
+
+      }
+        messageElement.innerHTML = currentMessage;
+
+
+  console.log(messageCall);
+  return messages
+  }
+
+
+  var messageCall = new XMLHttpRequest();
+  messageCall.addEventListener("load", loadMessages)
+  messageCall.open("GET", "messages.JSON");
+  messageCall.send();
+
+
+})(Chatty || {})
