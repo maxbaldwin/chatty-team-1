8070fffba2346ab3717f432ae3311c60c6e54b38
Bryan Hankins <bryanhankins@me.com>
2016-09-14 18:25:23 -0500

    included bootstrap fixes and syling

--- input.js
+++ input.js
@@ -15,10 +15,13 @@ var Chatty = (function(handleInput) {
     for (var i = msgCollection.length-1; i >= Math.max(msgCollection.length - 20, 0); i--){
       msgHTML += `<div id="msg-${i}" class="message1234">`;
         msgHTML += `<div class="name">${msgCollection[i].name}</div>`;
+        msgHTML += `<div class="chat">`;
+        msgHTML += `<div class="message bubble me">${msgCollection[i].userMessage}</div>`;
         msgHTML += `<div class="time">${msgCollection[i].time}</div>`;
-        msgHTML += `<div class="message">${msgCollection[i].userMessage}</div>`;
-        msgHTML += `<button class="btnDelete" id="btn-${i}">Delete</button>`;
-        msgHTML += `<button class="btnEdit" id="btnEdit-${i}">Edit</button>`;
+        msgHTML += `</div>`;
+        msgHTML += `<div class="delete-edit"><a href="#btnDelete" class="btnDelete" id="btn-${i}">Delete</a>`;
+        msgHTML += `<span> | </span>`;
+        msgHTML += `<a href="#btnEdit" class="btnEdit" id="btnEdit-${i}">Edit</a></div>`;
       msgHTML += `</div>`;
     }
     document.getElementById("messageOutput").innerHTML = msgHTML;
